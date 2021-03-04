ddb9c21984373f08cee4cf99276f82db49899df6
Moises Gonzalez <moises.gonzalez@flixbus.com>
2021-03-04 12:42:44 +0100

    Add system js

---
+++ gatsby-node.js
@@ -0,0 +1,10 @@
+/**
+ * Implement Gatsby's Node APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/node-apis/
+ */
+const { System, applyImportMap } = require('systemjs');
+
+exports.sourceNodes = async ({ actions, createNodeId, createContentDigest }) => {
+
+}
